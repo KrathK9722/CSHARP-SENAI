10eb3acf2d79bb2f8e8b4c80c7d4d8eced90dec6
Krath <arthurkochangasther@gmail.com>
2026-08-10 21:23:23 -0300

    a

---
+++ Activitie 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program // Objeto publico de classe Programa
+{
+    public static void Main() // Objeto publico estatico tipo void
+    {
+
+        float medium_stock, min_amount, max_amount;
+
+        Console.WriteLine("Welcome");
+        Console.Write("Write the minimum stock of this item: ");
+        while (!float.TryParse(Console.ReadLine(), out min_amount))
+        {
+
+            Console.WriteLine("Invalid number!");
+            Console.Write("Write the minimum stock of this item: ");
+
+        }
+        Console.WriteLine("Sucess minimium stock saved as: " + min_amount);
+        Console.Write("Write the maximium stock of this item: ");
+        while (!float.TryParse(Console.ReadLine(), out max_amount) || max_amount < min_amount)
+        {
+
+            Console.WriteLine("Invalid number!");
+            Console.Write("Write the maximum stock of this item: ");
+
+        }
+        Console.WriteLine("Sucess maximum stock saved as: " + max_amount);
+        Console.WriteLine("");
+        medium_stock = (min_amount + max_amount) / 2;
+        Console.WriteLine("The medium stock is: " + medium_stock);
+
+    }
+}
---
+++ Activitie 15/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Program // Objeto publico de classe Programa
+{
+    static int verify_quadrant(double A, double B)
+    {
+        int pos;
+        if (A > 0 & B > 0)
+        {
+            pos = 1;
+        }
+        else if (A < 0 & B > 0)
+        {
+            pos = 2;
+        }
+        else if (A < 0 & B < 0)
+        {
+            pos = 3;
+        }
+        else
+        {
+            pos = 4;
+        }
+        return pos;
+    }
+
+    public static void Main() // Objeto publico estatico tipo void
+    {
+
+        double x, y;
+
+        Console.WriteLine("Welcome");
+
+        Console.Write("Write the X position: ");
+        while (!double.TryParse(Console.ReadLine(), out x))
+        {
+
+            Console.WriteLine("Invalid number!");
+            Console.Write("Write the X position:  ");
+
+        }
+        Console.WriteLine("Sucess X position is " + x);
+
+        Console.Write("Write the Y position: ");
+        while (!double.TryParse(Console.ReadLine(), out y))
+        {
+
+            Console.WriteLine("Invalid number!");
+            Console.Write("Write the Y position: ");
+
+        }
+        Console.WriteLine("Sucess Y position saved: " + y);
+
+        int quadrant = verify_quadrant(x, y);
+
+        Console.WriteLine("The position is: " + x + "," + y + " and the quadrant is " + quadrant);
+
+    }
+}
